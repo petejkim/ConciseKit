3265ddb55292a41c808fb708cf176c4551031640
Peter Jihoon Kim <raingrove@gmail.com>
2010-12-01 19:48:13 +0800

    placed path specs under "path" describe block

--- ConciseKitSpecs/Spec/ConciseKitSpec.m
+++ ConciseKitSpecs/Spec/ConciseKitSpec.m
@@ -1,28 +1,34 @@
+/* ConciseKit
+ * Copyright 2010 Peter Jihoon Kim
+ * Licensed under the MIT License.
+ */
 #import "SpecHelper.h"
 #import "ConciseKit.h"
 
 DESCRIBE($) {
-  describe(@"+homePath", ^{
-    it(@"returns the user's home directory", ^{
-      assertThat([$ homePath], equalTo([@"~" stringByExpandingTildeInPath]));
+  describe(@"path", ^{
+    describe(@"+homePath", ^{
+      it(@"returns the user's home directory", ^{
+        assertThat([$ homePath], equalTo([@"~" stringByExpandingTildeInPath]));
+      });
     });
-  });
 
-  describe(@"+desktopPath", ^{
-    it(@"returns the user's desktop directory", ^{
-      assertThat([$ desktopPath], equalTo([@"~/Desktop" stringByExpandingTildeInPath]));
+    describe(@"+desktopPath", ^{
+      it(@"returns the user's desktop directory", ^{
+        assertThat([$ desktopPath], equalTo([@"~/Desktop" stringByExpandingTildeInPath]));
+      });
     });
-  });
 
-  describe(@"+documentPath", ^{
-    it(@"returns the user's document directory", ^{
-      assertThat([$ documentPath], equalTo([@"~/Documents" stringByExpandingTildeInPath]));
+    describe(@"+documentPath", ^{
+      it(@"returns the user's document directory", ^{
+        assertThat([$ documentPath], equalTo([@"~/Documents" stringByExpandingTildeInPath]));
+      });
     });
-  });
 
-  describe(@"+appPath", ^{
-    it(@"returns the app's directory", ^{
-      assertThat([$ appPath], endsWith(@"ConciseKitSpecs.app"));
+    describe(@"+appPath", ^{
+      it(@"returns the app's directory", ^{
+        assertThat([$ appPath], endsWith(@"ConciseKitSpecs.app"));
+      });
     });
   });
 }
