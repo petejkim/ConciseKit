355b407c22d53971664bfbac4908a146d58e205d
Basit Ali <onlybasit@gmail.com>
2013-04-15 17:02:55 +0500

    Add System Versioning Preprocessor Macros.
    Copied from https://gist.github.com/alex-cellcity/998472.

--- src/CKMacros.h
+++ src/CKMacros.h
@@ -47,3 +47,13 @@
 #define $size(val)        [NSValue valueWithSize:(val)]
 
 #define $safe(obj)        ((NSNull *)(obj) == [NSNull null] ? nil : (obj))
+
+/*
+ *  System Versioning Preprocessor Macros
+ */
+
+#define SYSTEM_VERSION_EQUAL_TO(v)                  ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] == NSOrderedSame)
+#define SYSTEM_VERSION_GREATER_THAN(v)              ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] == NSOrderedDescending)
+#define SYSTEM_VERSION_GREATER_THAN_OR_EQUAL_TO(v)  ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] != NSOrderedAscending)
+#define SYSTEM_VERSION_LESS_THAN(v)                 ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] == NSOrderedAscending)
+#define SYSTEM_VERSION_LESS_THAN_OR_EQUAL_TO(v)     ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] != NSOrderedDescending)
